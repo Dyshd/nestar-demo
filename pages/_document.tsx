8c72a31158eb6b46681aacbb39dd40b3ebd7c9d6
Muhammadjon <zoirovm823@gmail.com>
2025-12-01 19:13:44 +0500

    feat: MUI customized integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="" />
+      </Head>
       <body>
         <Main />
         <NextScript />
