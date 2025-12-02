2723603ebfcdc1c2a834c67fe1a5cfa65535524e
Muhammadjon <zoirovm823@gmail.com>
2025-12-02 12:15:15 +0500

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import { light } from "@/styles/MaterialTheme";
+import { light } from "@/scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
