708ae0af780645fa62c30a15321a5c3c595f2a78
Muhammadjon <zoirovm823@gmail.com>
2026-01-02 18:53:07 +0500

    feat: develop mobile version

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -6,21 +6,19 @@ import HeaderFilter from "../homepage/HeaderFilter";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+
 const withLayoutMain = (Component: any) => {
     return (props: any) => {
-        return (
-            <>
+        const device = useDeviceDetect();
+        if (device == "mobile") {
+            return (<>
                 <Head>
                     <title>Nestar</title>
                 </Head>
-                <Stack id="pc-wrap">
+                <Stack id="mobile-wrap">
                     <Stack id={"top"}>
                         <Top />
-                    </Stack>
-                    <Stack className={"header-main"}>
-                        <Stack className={"container"}>
-                            <HeaderFilter />
-                        </Stack>
                     </Stack>
                     <Stack id={"main"}>
                         <Component {...props} />
@@ -29,8 +27,33 @@ const withLayoutMain = (Component: any) => {
                         <Footer />
                     </Stack>
                 </Stack>
-            </>
-        );
+            </>)
+        } else {
+            return (
+                <>
+                    <Head>
+                        <title>Nestar</title>
+                    </Head>
+                    <Stack id="pc-wrap">
+                        <Stack id={"top"}>
+                            <Top />
+                        </Stack>
+                        <Stack className={"header-main"}>
+                            <Stack className={"container"}>
+                                <HeaderFilter />
+                            </Stack>
+                        </Stack>
+                        <Stack id={"main"}>
+                            <Component {...props} />
+                        </Stack>
+                        <Stack id={"footer"}>
+                            <Footer />
+                        </Stack>
+                    </Stack>
+                </>
+            );
+        }
+
     };
 };
 export default withLayoutMain;
